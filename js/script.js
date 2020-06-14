c1ed25ee800410e8c53d33d44a0ec1534839dd62
MamkinHazker <55155356+MamkinHazker@users.noreply.github.com>
2020-06-15 01:09:58 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,11 +1,53 @@
 let reviews = document.querySelectorAll('.reviews__review');
+let slArrows = document.querySelectorAll('.cards-slider-control');
+const anchors = document.querySelectorAll('a[href*="#"]');
+let burgerBtn = document.querySelector('.header__burger-btn');
 
-reviews.forEach(review => {
-    review.addEventListener('click', function(){
-        let cont = this.parentNode; 
-        try{document.querySelector('._active').querySelector('.reviews__video').pause();}
-        catch{}
-        document.querySelector('._active').className = document.querySelector('._active').className.replace('_active' , '');
-        cont.querySelector('.reviews__video-cont').className = cont.querySelector('.reviews__video-cont').className + ' _active';
+if(document.documentElement.clientWidth > 1100){
+    document.querySelectorAll('.reviews__video-cont')[5].className += ' _active';
+    reviews.forEach(review => {
+        review.addEventListener('click', function(){
+            let cont = this.parentNode; 
+            try{document.querySelector('._active').querySelector('.reviews__video').pause();}
+            catch{}
+            document.querySelector('._active').className = document.querySelector('._active').className.replace('_active' , '');
+            cont.querySelector('.reviews__video-cont').className = cont.querySelector('.reviews__video-cont').className + ' _active';
+        });
     });
+}
+slArrows.forEach(btn => {
+    btn.addEventListener('click', function(){
+        let i = 1;
+        let f = setInterval(() => {
+            if(i < 330){
+                btn.nextElementSibling.scrollBy(4, 0);
+                i+=4;
+            }else{
+                clearInterval(f);
+            }
+        }, 1);
+            
+    }); 
+});
+for (let anchor of anchors) {
+    anchor.addEventListener('click', function(e){
+        e.preventDefault();
+        let block = anchor.getAttribute('href');
+        document.querySelector('' + block).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
+
+burgerBtn.addEventListener('click', function(){
+    let menu = burgerBtn.nextElementSibling;
+    let links = menu.querySelectorAll('.links');
+    if(menu.style.height === '0px'){
+        menu.style.height = '117px';
+        links.forEach(link => { link.style.display = 'block'});   
+    }else{
+        menu.style.height = '0px';
+        links.forEach(link => { link.style.display = 'none'});
+    }
 });
